f34ba8c0a5c0f1e64f8c7a4baaf4f5824b946db1
Ercanincee <132362073+Ercanincee@users.noreply.github.com>
2023-11-10 20:10:35 +0300

    Add project files.

---
+++ FaktoriyelHesaplama/Program.cs
@@ -0,0 +1,17 @@
+﻿string faktoriyelstr;
+Console.WriteLine("bir sayi giriniz");
+int carpim = 1;
+
+faktoriyelstr = Console.ReadLine();
+if(!string.IsNullOrWhiteSpace(faktoriyelstr) )
+{
+    if(int.TryParse(faktoriyelstr,out int result)) {
+        for (int i = result;i>=1;i--)
+        {
+            carpim = carpim * i;
+        }
+        Console.WriteLine(carpim);
+
+    }
+    else { Console.WriteLine("lütfen gecerli bir sayi giriniz"); }
+}
